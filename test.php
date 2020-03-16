cab385479d08bf554085df68d5ca081ad9b698c0
Martin Hrůza <62236829+hruzic@users.noreply.github.com>
2020-03-16 09:46:46 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,19 @@
+<html lang="cs">
+    <head>
+        <meta name="hruzic">
+        <title>
+            _____
+        </title>
+        <link rel ="stylesheet" type ="text/css" href="css/screen.css">
+    </head>
+    <h1>Něco</h1>
+    <dl>
+        <dt>první termin</dt>
+        <dd>výklad terminu 1</dd>
+    </dl>
+    <dl>
+        <dt>prvni termin</dt>
+        <dd>vyklad terminu 1</dd>
+    </dl>
+
+    <a href="https://www.google.cz/%22%3EGoogle</a>    </html>
